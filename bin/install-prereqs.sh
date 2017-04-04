ed7a905eeb94fd575c9406d239544bc3a24895e5
yutongz <yutongz@google.com>
2017-04-04 20:40:42 +0000

    Add exec permission to install-prereqs and remove buildifier

--- bin/install-prereqs.sh
+++ bin/install-prereqs.sh
@@ -3,6 +3,3 @@
 # Install linters
 go get -u github.com/alecthomas/gometalinter
 gometalinter --install --vendored-linters
-
-# Install buildifier BUILD file validator
-go get -u github.com/bazelbuild/buildifier/buildifier
