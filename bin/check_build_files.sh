bf64cc9f6ac808fc274b4242732e60abf255ecc8
Yang Guan <yangguan@google.com>
2017-07-25 15:20:24 -0700

    fix script

--- bin/check_build_files.sh
+++ bin/check_build_files.sh
@@ -6,9 +6,7 @@ diff_size=$(gazelle -go_prefix istio.io/auth -mode diff | wc -c)
 
 if [ "$diff_size" -ne "0" ]; then
   echo "Some BUILD files are incorrect"
-  echo
-  gazelle -go_prefix istio.io/auth -mode diff
-  echo
   echo "Please run 'gazelle -go_prefix istio.io/auth -mode fix' and commit the changes"
+
   exit -1
 fi
