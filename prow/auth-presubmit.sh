6a9180ed7db9d51a18722194d8ca45ab6315e93c
Sebastien Vas <sebastienvas@google.com>
2017-07-25 22:36:00 -0700

    Fix link

--- prow/auth-presubmit.sh
+++ prow/auth-presubmit.sh
@@ -29,8 +29,7 @@ set -x
 if [ "${CI:-}" == 'bootstrap' ]; then
     # Test harness will checkout code to directory $GOPATH/src/github.com/istio/istio
     # but we depend on being at path $GOPATH/src/istio.io/istio for imports.
-    mkdir -p ${GOPATH}/src/istio.io
-    ln -s ${GOPATH}/src/github.com/istio/ ${GOPATH}/src/istio.io
+    ln -sf ${GOPATH}/src/github.com/istio ${GOPATH}/src/istio.io
     cd ${GOPATH}/src/istio.io/auth/
 
     # Use the provided pull head sha, from prow.
